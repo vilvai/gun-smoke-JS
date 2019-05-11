c819b578e8cb37bfb52a8da6976a6d2a6e6c75a7
Ville Vainio <ville.vainio1@gmail.com>
2019-05-11 16:07:40 +0300

    refactored code and added aim

--- app.js
+++ app.js
@@ -1,81 +1,28 @@
-class Player {
-  constructor(height, width, x) {
-    this.height = height;
-    this.width = width;
-    this.x = x;
-    this.y = 500;
-    this.xSpeed = 0;
-    this.ySpeed = 0;
-    this.isJumping = false;
-    this.acceleration = 0.5;
-    this.drag = 0.08;
-    this.maxSpeed = 8;
-    this.gravity = 0.5;
-    this.jumpPower = 14;
-  }
+let mouseX = 0;
+let mouseY = 0;
 
-  update(keys) {
-    if (keys.RIGHT) {
-      this.moveRight();
-    } else if (keys.LEFT) {
-      this.moveLeft();
-    } else {
-      this.xSpeed *= 1 - this.drag;
-      if (Math.abs(this.xSpeed) < 0.1) this.xSpeed = 0;
-    }
-    if (keys.UP && !this.isJumping) {
-      this.jump();
-    }
-    if (this.y + this.ySpeed > 600) {
-      this.y = 600;
-      this.ySpeed = 0;
-      this.isJumping = false;
-    } else {
-      this.ySpeed += this.gravity;
-    }
-    this.y += this.ySpeed;
-    this.x += this.xSpeed;
-  }
-
-  moveRight() {
-    if (this.xSpeed < 0) this.xSpeed /= 2;
-    this.xSpeed = Math.min(
-      Math.max(-this.maxSpeed, this.xSpeed + this.acceleration),
-      this.maxSpeed
-    );
-  }
-
-  moveLeft() {
-    if (this.xSpeed > 0) this.xSpeed /= 2;
-    this.xSpeed = Math.min(
-      Math.max(-this.maxSpeed, this.xSpeed - this.acceleration),
-      this.maxSpeed
-    );
-  }
-
-  jump() {
-    this.ySpeed = -this.jumpPower;
-    this.isJumping = true;
-  }
-
-  draw(context) {
-    context.fillStyle = '#000';
-    context.fillRect(this.x, this.y, this.width, this.height);
-  }
-}
+const onMouseMove = event => {
+  mouseX = event.pageX;
+  mouseY = event.pageY;
+};
 
 Sketch.create({
   container: document.getElementById('container'),
   setup() {
-    this.player = new Player(100, 40, this.width / 2);
+    this.player = new Player(100, 40, this.width / 4);
+    this.aim = new Aim();
   },
   mousemove() {},
   update() {
     this.player.update(this.keys);
+    this.aim.update(mouseX, mouseY);
   },
   draw() {
     this.fillStyle = '#ccc';
     this.fillRect(0, 0, this.width, this.height);
     this.player.draw(this);
+    this.aim.draw(this);
   }
 });
+
+document.addEventListener('mousemove', onMouseMove);
---
+++ player.js
@@ -0,0 +1,65 @@
+class Player {
+  constructor(height, width, x) {
+    this.height = height;
+    this.width = width;
+    this.x = x;
+    this.y = 500;
+    this.xSpeed = 0;
+    this.ySpeed = 0;
+    this.isJumping = false;
+    this.acceleration = 0.5;
+    this.drag = 0.08;
+    this.maxSpeed = 8;
+    this.gravity = 0.5;
+    this.jumpPower = 14;
+  }
+
+  update(keys) {
+    if (keys.RIGHT) {
+      this.moveRight();
+    } else if (keys.LEFT) {
+      this.moveLeft();
+    } else {
+      this.xSpeed *= 1 - this.drag;
+      if (Math.abs(this.xSpeed) < 0.1) this.xSpeed = 0;
+    }
+    if (keys.UP && !this.isJumping) {
+      this.jump();
+    }
+    if (this.y + this.ySpeed > 600) {
+      this.y = 600;
+      this.ySpeed = 0;
+      this.isJumping = false;
+    } else {
+      this.ySpeed += this.gravity;
+    }
+    this.y += this.ySpeed;
+    this.x += this.xSpeed;
+  }
+
+  moveRight() {
+    if (this.xSpeed < 0) this.xSpeed /= 2;
+    this.xSpeed = Math.min(
+      Math.max(-this.maxSpeed, this.xSpeed + this.acceleration),
+      this.maxSpeed
+    );
+  }
+
+  moveLeft() {
+    if (this.xSpeed > 0) this.xSpeed /= 2;
+    this.xSpeed = Math.min(
+      Math.max(-this.maxSpeed, this.xSpeed - this.acceleration),
+      this.maxSpeed
+    );
+  }
+
+  jump() {
+    this.ySpeed = -this.jumpPower;
+    this.isJumping = true;
+  }
+
+  draw(context) {
+    context.fillStyle = '#000';
+    context.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
