5ddc91fbfae5c228bd0efb4aa05a0d42da2db112
Ville Vainio <ville.vainio1@gmail.com>
2019-05-11 18:51:01 +0300

    added aim

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let mouseX = 0;
-let mouseY = 0;
+let mouseX;
+let mouseY;
 
 const onMouseMove = event => {
   mouseX = event.pageX;
@@ -10,18 +10,14 @@ Sketch.create({
   container: document.getElementById('container'),
   setup() {
     this.player = new Player(100, 40, this.width / 4);
-    this.aim = new Aim();
   },
-  mousemove() {},
   update() {
-    this.player.update(this.keys);
-    this.aim.update(mouseX, mouseY);
+    this.player.update(this.keys, mouseX, mouseY);
   },
   draw() {
     this.fillStyle = '#ccc';
     this.fillRect(0, 0, this.width, this.height);
     this.player.draw(this);
-    this.aim.draw(this);
   },
 });
 
--- player.js
+++ player.js
@@ -12,9 +12,18 @@ class Player {
     this.maxSpeed = 8;
     this.gravity = 0.5;
     this.jumpPower = 14;
+    this.armLength = 50;
   }
 
-  update(keys) {
+  getCenterX() {
+    return this.x + this.width / 2;
+  }
+
+  getCenterY() {
+    return this.y + this.height / 2;
+  }
+
+  update(keys, mouseX, mouseY) {
     if (keys.RIGHT) {
       this.moveRight();
     } else if (keys.LEFT) {
@@ -35,6 +44,9 @@ class Player {
     }
     this.y += this.ySpeed;
     this.x += this.xSpeed;
+
+    this.mouseX = mouseX;
+    this.mouseY = mouseY;
   }
 
   moveRight() {
@@ -61,5 +73,37 @@ class Player {
   draw(context) {
     context.fillStyle = '#000';
     context.fillRect(this.x, this.y, this.width, this.height);
+
+    if (this.mouseX) {
+      context.lineWidth = 1;
+      context.strokeStyle = '#f00';
+      context.beginPath();
+      context.moveTo(this.getCenterX(), this.getCenterY());
+      context.lineTo(this.mouseX, this.mouseY);
+      context.stroke();
+
+      const deltaX = this.mouseX - this.getCenterX();
+      const deltaY = this.mouseY - this.getCenterY();
+
+      const angle = Math.atan(deltaY / deltaX);
+
+      let armEndX = Math.cos(angle);
+      let armEndY = Math.sin(angle);
+
+      if (deltaX < 0) {
+        armEndX = -armEndX;
+        armEndY = -armEndY;
+      }
+
+      context.lineWidth = 15;
+      context.strokeStyle = '#000';
+      context.beginPath();
+      context.moveTo(this.getCenterX(), this.getCenterY());
+      context.lineTo(
+        this.getCenterX() + armEndX * this.armLength,
+        this.getCenterY() + armEndY * this.armLength
+      );
+      context.stroke();
+    }
   }
 }
