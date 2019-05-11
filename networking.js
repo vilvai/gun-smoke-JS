bf7bb37db09916ab29ab33166448caf6437ddb28
od <otsopohjola95@gmail.com>
2019-05-11 21:23:59 +0300

    networking changes

---
+++ networking.js
@@ -0,0 +1,5 @@
+const peer = new Peer({key: 'peerjs', host: 'gunsmok.herokuapp.com', secure: true, port: 443});
+
+console.log(peer)
+
+//heroku.com/deploy/?template=https://github.com/peers/peerjs-server
