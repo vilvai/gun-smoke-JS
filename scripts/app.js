e973736b08c106e53cabf493d468b10b00c9e0df
od <otsopohjola95@gmail.com>
2019-05-14 18:48:20 +0300

    Trying this format, can override if u want to

--- scripts/app.js
+++ scripts/app.js
@@ -32,8 +32,6 @@ ctx.update = ()=>{
     //send_to_clients(data)
 }
 
-
-
 ctx.draw = ()=> {
   ctx.fillStyle = "#ccc";
   ctx.fillRect(0, 0, ctx.width, ctx.height);
@@ -50,3 +48,37 @@ ctx.spawn = ()=>{
 }
 
 document.addEventListener("mousemove", onMouseMove);
+
+/* let mouseX;
+let mouseY;
+
+const onMouseMove = event => {
+  mouseX = event.pageX;
+  mouseY = event.pageY;
+};
+
+Sketch.create({
+  container: document.getElementById("container"),
+  setup() {
+    this.player = new Player(100, 40, this.width / 4);
+    this.platforms = [
+      new Platform(50, 300, 200, 700),
+      new Platform(50, 300, 700, 700),
+      new Platform(50, 300, 1040, 700),
+      new Platform(500, 50, 1300, 200),
+      new Platform(50, 300, 700, 450)
+    ];
+  },
+  update() {
+    this.player.update(this.keys, this.platforms, this.height, mouseX, mouseY);
+  },
+  draw() {
+    this.fillStyle = "#ccc";
+    this.fillRect(0, 0, this.width, this.height);
+    this.platforms.forEach(x => x.draw(this));
+    this.player.draw(this);
+  }
+});
+
+document.addEventListener("mousemove", onMouseMove);
+ */
